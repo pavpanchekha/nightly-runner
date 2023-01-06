51ba85fc5dfc7fe24e2dd2a0b852657c2df9b2cc
Pavel Panchekha <me@pavpanchekha.com>
2023-01-06 13:43:29 -0700

    Switch to argparse

--- cmdline.py
+++ cmdline.py
@@ -5,7 +5,7 @@ import os
 import nightlies
 from pathlib import Path
 
-def run():
+def load():
     CONF_FILE = os.getenv("NIGHTLY_CONF_FILE")
     assert CONF_FILE, "ERROR: could not find $NIGHTLY_CONF_FILE environment variable"
     
@@ -16,32 +16,50 @@ def run():
     runner.load()
     os.chdir(old_cwd)
     
-    _, cmd, *args = sys.argv
+    return runner
+
+def info(runner, args):
+    print(f"dir={runner.dir}")
+    print(f"config_file={runner.config_file}")
+    print(f"log_dir={runner.log_dir}")
+    print(f"pid_file={runner.pid_file}")
+    print(f"info_file={runner.info_file}")
+
+def emoji(runner, args):
+    runner.add_info("emoji", arg.emoji)
+
+def url(runner, args):
+    runner.add_info("url", arg.url)
+
+def img(runner, args):
+    runner.add_info("img", arg.url)
     
-    if cmd == "info":
-        assert len(args) == 0, "usage: nightly-resutls info"
-        print(f"dir={runner.dir}")
-        print(f"config_file={runner.config_file}")
-        print(f"log_dir={runner.log_dir}")
-        print(f"pid_file={runner.pid_file}")
-        print(f"info_file={runner.info_file}")
+def valid_url(s):
+    if "://" in args[0]:
         return
-    elif cmd == "emoji":
-        assert len(args) == 1, "usage: nightly-results emoji <emoji name>"
-    elif cmd == "url":
-        assert len(args) == 1, "usage: nightly-results url <url>"
-        assert "://" in args[0], "ERROR: <url> must have format http[s]://..."
-    elif cmd == "img":
-        assert len(args) == 1, "usage: nightly-results img <url>"
-        assert "://" in args[0], "ERROR: <url> must have format http[s]://..."
     else:
-        assert False, f"{cmd} is not a known nightly-results command"
-    
-    runner.add_info(cmd, *args)
+        raise ValueError("ERROR: <url> must have format http[s]://...")
 
 if __name__ == "__main__":
-    try:
-        run()
-    except AssertionError as e:
-        print(e)
-        sys.exit(1)
+    import argparse
+    parser = argparse.ArgumentParser(prog="nightly-results")
+    subparsers = parser.add_subparsers()
+
+    info_p = subparser.add_parser("info", help="Show path information for the nightlies")
+    info_p.set_defaults(func, info)
+
+    emoji_p = subparser.add_parser("emoji", help="Show an emoji in Slack")
+    emoji_p.add_argument("emoji")
+    info_p.set_defaults(func, emoji)
+
+    url_p = subparser.add_parser("url", help="Output to link to in Slack")
+    url_p.add_argument("url", type=valid_url)
+    info_p.set_defaults(func, url)
+
+    img_p = subparser.add_parser("img", help="Image to show in Slack")
+    img_p.add_argument("url", type=valid_url)
+    info_p.set_defaults(func, url)
+
+    args = parser.parse_args()
+    runner = load()
+    args.func(runner, args)
