ee2c05aba8d22d35f563fcc7654d8f0b464a60a6
Pavel Panchekha <me@pavpanchekha.com>
2023-04-18 17:12:40 -0600

    Forgot to commit JS file

---
+++ views/nightly.js
@@ -0,0 +1,51 @@
+class OpenProjects {
+    constructor(lst) {
+        this.open_projects = lst;
+    }
+
+    read() {
+        let projects = document.querySelectorAll(".repo");
+        for (let project of projects) {
+            let name = project.querySelector("kbd.name").textContent;
+            project.open = this.open_projects.indexOf(name) >= 0;
+        }
+    }
+
+    write() {
+        this.open_projects.length = 0;
+        let projects = document.querySelectorAll(".repo");
+        for (let project of projects) {
+            let name = project.querySelector("kbd.name").textContent;
+            if (project.open) {
+                this.open_projects.push(name);
+            }
+        }
+    }
+
+    static load() {
+        if (window.localStorage["open-projects"]) {
+            return OpenProjects(window.localStorage["open-projects"].split(" "));
+        } else {
+            return OpenProjects([]);
+        }
+    }
+
+    save() {
+        window.localStorage["open-projects"] = this.open_projects.join(" ");
+    }
+}
+
+const OPEN_PROJECTS = null;
+
+window.addEventListener("DOMContentLoaded", () => {
+    OPEN_PROJECTS = OpenProjects.load();
+    OPEN_PROJECTS.read();
+
+    let projects = document.querySelectorAll(".repo");
+    for (let project of projects) {
+        project.addEventListener("toggle" () => {
+            OPEN_PROJECTS.write();
+            OPEN_PROJECTS.save();
+        });
+    }
+});
